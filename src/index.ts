086589ebe3712884683a5076c3ff1521abd89144
dacevedom <d.acevedom1993@gmail.com>
2025-10-02 16:24:34 -0300

    Scaffolded with webpack version

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-// TODO: Add TS code here...
-console.log("This is TS");
+const inp = document.getElementById("Password") as HTMLInputElement | null;
+const btn = document.getElementById("toggleVisibility") as HTMLButtonElement | null;
+
+if (inp && btn) {
+    btn.addEventListener("click", () => {
+        if (inp.type === "password") {
+            inp.type = "text";
+            btn.textContent = "Shown";
+        } else {
+            inp.type = "password";
+            btn.textContent = "Hidden";
+        }
+    });
+}
