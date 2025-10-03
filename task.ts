74977cd81e25fca7de712571827844ce0e756269
dacevedom <d.acevedom1993@gmail.com>
2025-10-03 12:06:04 -0300

    Added CSS fixes to showPassword button and corrected TS call to password field

--- task.ts
+++ task.ts
@@ -1,14 +1,14 @@
-const inp = document.getElementById("Password") as HTMLInputElement | null;
+const inp = document.getElementById("password") as HTMLInputElement | null;
 const btn = document.getElementById("toggleVisibility") as HTMLButtonElement | null;
 
 if (inp && btn) {
     btn.addEventListener("click", () => {
         if (inp.type === "password") {
             inp.type = "text";
-            btn.textContent = "Shown";
+            btn.textContent = "🙈";
         } else {
             inp.type = "password";
-            btn.textContent = "Hidden";
+            btn.textContent = "👁️";
         }
     });
 }
